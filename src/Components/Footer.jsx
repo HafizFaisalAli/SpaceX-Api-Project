b050cfd78964e1e96aeea6130b08e97e5490db3f
hafizfaisalali <hafizfaisalali605@gmail.com>
2024-12-23 12:11:50 +0500

    feat: burako logo add

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import BurakoLogo from '../assets/Media/BurakoSolutions.png';
 
 const Footer = () => {
   return (
     <>
-      <footer className=" py-3 text-center footer-shadow" title="Create By Hafiz Faisal Ali">
-        All Rights Reserved &copy;{new Date().getFullYear()}
+      <footer className=" py-3 text-center footer-shadow">
+        <a
+          href="https://www.burakosolutions.com/"
+          className="d-flex flex-col justify-content-center align-items-center text-decoration-none text-hover"
+          target="blank"
+        >
+          <strong className="font-normal text-lg text-slate-400 hover:text-white">
+            Powered by Burako Solutions
+          </strong>
+          <img src={BurakoLogo} alt="Burako Solutions" width={40} height={40} />
+        </a>
       </footer>
     </>
   );
